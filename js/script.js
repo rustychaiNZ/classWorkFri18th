ba573fc944b3fb6b3b750de0caea75a3c54eecab
Russell grant Johnson <932400859@bun2-206-14.local>
2019-10-18 15:05:05 +1300

    work completed in class

---
+++ js/script.js
@@ -0,0 +1,89 @@
+console.log('Class Exercises');
+
+// Blank variables for the user's inputted string
+var givenString = 0;
+var reversedString = 0;
+
+document.getElementById('submitString').addEventListener('click', function() {
+	// Takes the inputted string from the user and stores it in the 'givenString' variable
+	givenString = document.getElementById('userString').value;
+	// Converts the given string into lower case characters
+	var givenStringLower = givenString.toLowerCase();
+	console.log(givenString);
+
+	// Takes the users string and reverses it
+	// Takes user input and turns it into an array
+	reversedString = givenStringLower.split('');
+	// Takes the array and reverses it
+	reversedString = reversedString.reverse();
+	// Takes the reversed array and joins it together and converts it into a string
+	reversedString = reversedString.join('');
+	console.log(reversedString);
+
+	// Checks to see if the given string is the same as the reversed version
+	if (givenStringLower == reversedString) {
+		console.log('The word is a Palidrome');
+		// outputs the word how the user has written it if it is a palidrone
+		document.getElementById('stringOutput').innerHTML = '<b>' + givenString + '</b> is a Palidrome';
+	} else {
+		document.getElementById('stringOutput').innerHTML = '<b>' + givenString + '</b> is not a Palidrome';
+		// outputs the word how the user has written it if it is not a palidrone
+		console.log('The word is not a Palidrome');
+	}
+});
+
+
+// Find out whether a users inputted number is odd or even
+var x = 0;
+// Finding out whether a number is an odd or even
+document.getElementById('submitOddOrEven').addEventListener('click', function() {
+	// Takes string entered by user in field and converts to a interger value
+	x = parseInt(document.getElementById('userOddOrEven').value);
+	console.log(x);
+
+	// If the interger can be divided by 2 with no remainder, then it is an even number
+	if(x % 2 === 0) {
+		document.getElementById('resultOddOrEven').innerHTML = 'Your number <b>' + x + '</b> is divsable by 2';
+	} 
+	// If the number cannot be devided by 2 with no remainder, then it is not an even number
+	else {
+		document.getElementById('resultOddOrEven').innerHTML = 'Your number <b>' + x + '</b> is NOT divsable by 2';
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
